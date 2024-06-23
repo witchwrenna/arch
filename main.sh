046eeabec703ba79a4dec9b716267391b0703fa7
Witch Wrenna <witchwrenna@gmail.com>
2024-06-23 00:39:53 -0400

    enabling script

--- main.sh
+++ main.sh
@@ -8,8 +8,8 @@ cd arch-main
 
 #commenting out to avoid accidently running before it's all ready
 
-#sh installarch.sh
-#arch-chroot /mnt /configarch.sh
-#mkdir -p /mnt/home/lilith/.config/hypr/
-#mv /config/hyprland.conf /mnt/home/lilith/.config/hypr/hyprland.conf
+sh installarch.sh
+arch-chroot /mnt /configarch.sh
+mkdir -p /mnt/home/lilith/.config/hypr/
+mv /config/hyprland.conf /mnt/home/lilith/.config/hypr/hyprland.conf
 #arch-chroot /mnt /installgit.sh
