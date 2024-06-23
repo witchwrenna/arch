4ae31ae7acaadef51135b5f8db797943905b2732
discosheep <131913155+discosheep@users.noreply.github.com>
2024-06-23 01:56:10 -0400

    fixed some directory messups

--- main.sh
+++ main.sh
@@ -3,19 +3,18 @@
 curl -LO https://github.com/witchwrenna/arch/archive/master.zip
 pacman -Sy unzip --noconfirm --needed
 unzip ~/master.zip
-cd arch-main
-sh installarch.sh
+sh arch-main/installarch.sh
 
 #Copy over scripts to prepare for chroot
-mv configarch.sh /mnt/configarch.sh
-mv installgit.sh /mnt/installgit.sh
+mv arch-main/configarch.sh /mnt/configarch.sh
+mv arch-main/installgit.sh /mnt/installgit.sh
 
 #Customize the OS!
 arch-chroot /mnt sh configarch.sh
 
 #Copying over basic config stuff needed for nvidia support
 mkdir -p /mnt/home/lilith/.config/hypr/
-mv /config/hyprland.conf /mnt/home/lilith/.config/hypr/hyprland.conf
+mv arch-main/config/hyprland.conf /mnt/home/lilith/.config/hypr/hyprland.conf
 
 #Get dotfiles sync support up
 arch-chroot /mnt sh /installgit.sh
