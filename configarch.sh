e12fd69f51ca1b5e3f9e72c6e5da7e05f0962183
Witch Wrenna <witchwrenna@gmail.com>
2024-06-23 05:46:40 -0400

    removing grub to test refind

--- configarch.sh
+++ configarch.sh
@@ -65,18 +65,20 @@ pacman -S nvidia nvidia-utils nvidia-settings lib32-nvidia-utils libva-nvidia-dr
 echo "-------------------------------------------------"
 echo "Setting up grub"
 echo "-------------------------------------------------"
-pacman -S grub efibootmgr dosfstools mtools os-prober --noconfirm --needed
-#need to mount windows to see stuff
-mount /dev/disk/by-id/nvme-eui.002538592140e412-part2 /mnt/win11
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="DemonBoot"
-sed -i 's/^#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
-sed -i '/.*^GRUB_CMDLINE_LINUX_DEFAULT=.*/ c\GRUB_CMDLINE_LINUX_DEFAULT="quiet loglevel=3 nvidia_drm.modeset=1/"' /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
+# pacman -S grub efibootmgr dosfstools mtools os-prober --noconfirm --needed
+# #need to mount windows to see stuff
+# mount /dev/disk/by-id/nvme-eui.002538592140e412-part2 /mnt/win11
+# grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="DemonBoot"
+# sed -i 's/^#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
+# sed -i '/.*^GRUB_CMDLINE_LINUX_DEFAULT=.*/ c\GRUB_CMDLINE_LINUX_DEFAULT="quiet loglevel=3 nvidia_drm.modeset=1/"' /etc/default/grub
+# grub-mkconfig -o /boot/grub/grub.cfg
 
-
+read "trying out refind press enter"
 
 pacman -S refind efibootmgr refind-install
 refind-install
+
+read "press enter to continue"
 
 echo "-------------------------------------------------"
 echo "Installing wayland + hyprland"
