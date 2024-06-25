70662e7501f3b150a122183c990f7127111bf1e4
discosheep <131913155+discosheep@users.noreply.github.com>
2024-06-25 13:42:45 -0400

    beginning documentation + notes

---
+++ createiso.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#createiso
+#use archiso
+#create custom iso that includes the following changes:
+#short password for ssh access (insecure but i'm behind a nat so lol)
+#main.sh included already
+#just thinking out loud
---
+++ readme.md
@@ -0,0 +1,3 @@
+Copy main.sh into your live iso and run
+
+optionally, run createiso on an arch install and boot off of that, it will have main.sh included. NOT WORKING YET.
